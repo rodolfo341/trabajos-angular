8f5c4ea96619a42d1a9ef04e2268b481a8f25db6
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-08-28 19:35:28 -0400

    agregamos el componente persona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PersonasComponent } from './personas/personas.component';
+import { PersonaComponent } from './persona/persona.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
-        PersonasComponent
+        PersonasComponent,
+        PersonaComponent
     ],
     imports: [
         BrowserModule
