7ee93a8d2a20c111dc0f5f35dd4e586de162994b
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-08-28 23:15:33 -0400

    se agrega bootstrap con mpm

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-personas',                      
-  templateUrl: './personas.component.html',      
-  styleUrls: ['./personas.component.css']
+    selector: 'app-personas',
+    templateUrl: './personas.component.html',
+    styleUrls: ['./personas.component.css']
 })
 export class PersonasComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
+
